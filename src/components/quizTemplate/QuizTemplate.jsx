d43e501f5cf1723f2b5cf5928e77284876530fae
Danil Gribanov <gribanovwork@yandex.ru>
2023-02-06 02:19:53 +0500

    feat: сделал заготовку квиза и примерную разметку

---
+++ src/components/quizTemplate/QuizTemplate.jsx
@@ -0,0 +1,25 @@
+import quizStyles from "./quizTemplate.module.css"
+export const QuizTemplate = ({quizData}) => {
+    return(
+        <div className={quizStyles.quizBox} id="quiz">
+            <div className={quizStyles.quizHeader}>
+                <h2 id="question">Question text</h2>
+                <ul>
+                    <li>
+                        <input type="radio" name="answer" id="a" className="answer"/>
+                            <label htmlFor="a" id="a_text">Question</label>
+                    </li>
+                    <li>
+                        <input type="radio" name="answer" id="b" className="answer"/>
+                            <label htmlFor="b" id="b_text">Question</label>
+                    </li>
+                    <li>
+                        <input type="radio" name="answer" id="c" className="answer"/>
+                            <label htmlFor="c" id="c_text">Question</label>
+                    </li>
+                </ul>
+            </div>
+            <button id="submit">Submit</button>
+        </div>
+    )
+}
---
+++ src/utils/quizData.js
@@ -0,0 +1,23 @@
+export const quizData = [
+    {
+        question: "А?",
+        a: "А",
+        b: "Б",
+        c: "В",
+        correct: "a",
+    },
+    {
+        question: "Б?",
+        a: "А",
+        b: "Б",
+        c: "В",
+        correct: "b",
+    },
+    {
+        question: "В?",
+        a: "А",
+        b: "Б",
+        c: "В",
+        correct: "c",
+    },
+];
