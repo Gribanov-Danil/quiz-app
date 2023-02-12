476eb90ed02d566009a8d0b1c87ded2f3de95116
Danil Gribanov <gribanovwork@yandex.ru>
2023-02-12 12:23:43 +0500

    feat: убрал дефолтную разметку и поставил квиз по центру

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import {QuizTemplate} from "./components/quizTemplate/QuizTemplate";
 import {quizData} from "./utils/quizData";
@@ -7,20 +6,6 @@ import {quizData} from "./utils/quizData";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
       <QuizTemplate quizData={quizData}/>
     </div>
   );
--- src/components/quizTemplate/QuizTemplate.jsx
+++ src/components/quizTemplate/QuizTemplate.jsx
@@ -1,25 +1,29 @@
 import quizStyles from "./quizTemplate.module.css"
+
 export const QuizTemplate = ({quizData}) => {
     return(
-        <div className={quizStyles.quizBox} id="quiz">
-            <div className={quizStyles.quizHeader}>
-                <h2 id="question">Question text</h2>
-                <ul>
-                    <li>
-                        <input type="radio" name="answer" id="a" className="answer"/>
+        <div className={quizStyles.quiz_layout}>
+            <div className={quizStyles.quiz_box} id="quiz">
+                <div className={quizStyles.quiz_header}>
+                    <h2 id="question">Question text</h2>
+                    <ul>
+                        <li>
+                            <input type="radio" name="answer" id="a" className="answer"/>
                             <label htmlFor="a" id="a_text">Question</label>
-                    </li>
-                    <li>
-                        <input type="radio" name="answer" id="b" className="answer"/>
+                        </li>
+                        <li>
+                            <input type="radio" name="answer" id="b" className="answer"/>
                             <label htmlFor="b" id="b_text">Question</label>
-                    </li>
-                    <li>
-                        <input type="radio" name="answer" id="c" className="answer"/>
+                        </li>
+                        <li>
+                            <input type="radio" name="answer" id="c" className="answer"/>
                             <label htmlFor="c" id="c_text">Question</label>
-                    </li>
-                </ul>
+                        </li>
+                    </ul>
+                </div>
+                <button id="submit">Submit</button>
             </div>
-            <button id="submit">Submit</button>
         </div>
+
     )
 }
